8d54df2df1267e243d137be47e43d7ae7bf302ae
Thanat H <kendozx@gmail.com>
2019-02-01 11:19:32 +0700

    Custom Webhok V1.45 - Register Webview

--- routes/webviews.js
+++ routes/webviews.js
@@ -2,11 +2,20 @@
 
 const config = require('../config');
 const express = require('express');
+const fbservice = require('../services/fb-service');
 
 const router = express.Router();
 
 router.get('/webview', function (req, res){
     res.render('sfuser-register');
 });
+
+
+router.get('/save', function (req, res){
+    let body = req.body;
+    let response = `${body.sfinput} psid = ${body.psid}`;
+    fbservice.sendTextMessage(body.psid, response);
+});
+
 
 module.exports = router;
